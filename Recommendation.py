08296d8550af60ae825edc655c0f4c2eae0fdee4
konyeinmg <nyeinchanaung.myeik@gmail.com>
2023-12-03 09:26:47 +0630

    extract feature with TF IDF

--- Recommendation.py
+++ Recommendation.py
@@ -3,3 +3,8 @@ import services
 data = services.readData('data.json')
 #print(len(data))
 #print(data[:5])
+
+num_features = 1024
+noise = 10
+tfidf_vectorizer, tfidf_matrix = services.getFeatures(data, num_features, noise)
+
--- services.py
+++ services.py
@@ -1,5 +1,8 @@
 import json
-import numpy as np 
+import numpy as np
+import pandas as pd 
+
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 def readData(url):
     f = open(url)
@@ -11,3 +14,28 @@ def readData(url):
         contents.append(item.get('content'))
 
     return contents
+
+def getFeatures(data, num_features = 128, noise = 10, log = False):
+    tfidf_vectorizer = TfidfVectorizer(max_features=num_features, min_df=noise ,stop_words='english')
+    tfidf_matrix = tfidf_vectorizer.fit_transform(data)
+    if log:
+        features = tfidf_vectorizer.get_feature_names_out()
+        tf = pd.DataFrame(tfidf_matrix.toarray(), columns = features)
+        print(tf)
+    return tfidf_vectorizer,tfidf_matrix
+
+#return number of words
+def numberOfWords(data):
+    words = set()
+    for item in data:
+        for word in item.split():
+            words.add(word)
+    return len(words)
+
+#return a dictionary of each word with its occurence in the corpus
+def wordCount(data):
+    words = {}
+    for item in data:
+        for word in item.split():
+            words[word] = words.get('word',0) + 1
+    return words

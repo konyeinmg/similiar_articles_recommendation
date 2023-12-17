c566db61bedf9bf0f755a7136b43a04df05307e8
konyeinmg <nyeinchanaung.myeik@gmail.com>
2023-12-17 14:37:36 +0630

    add comments in recommendation file

--- Recommendation.py
+++ Recommendation.py
@@ -38,7 +38,7 @@ document_embeddings = np.vstack(document_embeddings)
 services.logger('Document Embeddings Calculated')
 
 
-candidate = test[200]
+candidate = test[201] #change id of document u want to calculate
 services.logger(candidate)
 test_tfidf = tfidf_vectorizer.transform([candidate])
 test_document_embedding = services.getDocumentEmbeddings(candidate, 0, word_embeddings, test_tfidf, tfidf_vectorizer)
@@ -46,10 +46,10 @@ test_document_embedding = services.getDocumentEmbeddings(candidate, 0, word_embe
 services.logger('Similarity Scores Calculating....')
 services.logger('Similar Articles.........')
 similar_articles, indices = Similarlity.getSimilarArticles(test_document_embedding, document_embeddings, train)
-print('Similar articles indices by pure cosine similarity : ', indices)
+print('Similar articles indices by pure cosine similarity : ', indices) #you can print similar articles
 
 #LSH
 lsh = LSH()
 lsh.make_hash_table(document_embeddings)
-lsh_similar_articles, lsh_indices = lsh.similar_articles(test_document_embedding, train)
-print('Similar articles indices by lsh : ', lsh_indices)
+lsh_similar_articles, lsh_indices = lsh.similar_articles(test_document_embedding, train) #you can print similar articles
+print('Similar articles indices by LSH : ', lsh_indices)

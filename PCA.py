58dabaf5e87d67ceedd1ec1af61427c95f93e42c
konyeinmg <nyeinchanaung.myeik@gmail.com>
2023-12-18 16:02:48 +0630

    progress: pca fit transform

--- PCA.py
+++ PCA.py
@@ -1,4 +1,15 @@
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+import numpy as np
+
 import services
 
-word_embeddings = services.getWordEmbedding('GoogleNews-vectors-negative300.bin')
+word_embeddings_dict = services.getWordEmbedding('GoogleNews-vectors-negative300.bin')
 services.logger('Word Embeddings Loaded')
+
+embeddings = list(word_embeddings_dict.values())
+
+word_embeddings = np.array(embeddings)
+
+pca = PCA(n_components=2)
+word_embeddings_reduced = pca.fit_transform(word_embeddings)

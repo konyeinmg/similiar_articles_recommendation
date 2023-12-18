f18473a64e9289088b961f3de97c9cb33eadb7e7
konyeinmg <nyeinchanaung.myeik@gmail.com>
2023-12-18 17:31:46 +0630

    plot word embedding with pca transfom

--- PCA.py
+++ PCA.py
@@ -7,9 +7,9 @@ import services
 word_embeddings_dict = services.getWordEmbedding('GoogleNews-vectors-negative300.bin')
 services.logger('Word Embeddings Loaded')
 
-plt.figure(figsize=(10,8))
+plt.figure(figsize=(5,4))
 
-opposite_pairs = [('good', 'bad')]
+opposite_pairs = [('good', 'bad'), ('king', 'queen'), ('hot', 'cold')]
 
 for pair in opposite_pairs:
     word1 = word_embeddings_dict[pair[0]]
@@ -18,10 +18,12 @@ for pair in opposite_pairs:
     #print(word_embeddings)
     pca = PCA(n_components=2)
     word_embeddings_reduced = pca.fit_transform(word_embeddings)
-    #emb1 = word_embeddings_reduced[list(word_embeddings_dict.keys()).index(pair[0])]
-    #emb2 = word_embeddings_reduced[list(word_embeddings_dict.keys()).index(pair[1])]
-    plt.scatter(word_embeddings_reduced[0][0], word_embeddings_reduced[0][1], marker='x', color='red', s=100, label = pair[0])
-    plt.scatter(word_embeddings_reduced[1][0], word_embeddings_reduced[1][1], marker='x', color='green', s=100, label = pair[1])
+    emb1 = word_embeddings_reduced[0]
+    emb2 = word_embeddings_reduced[1]
+    plt.scatter(emb1[0],emb1[1], marker='x', color='red', s=100)
+    plt.scatter(emb2[0],emb2[1], marker='x', color='green', s=100)
+    plt.text(emb1[0],emb1[1], pair[0], fontsize=8, ha='right', va='bottom', color='blue')
+    plt.text(emb2[0],emb2[1], pair[1], fontsize=8, ha='right', va='bottom', color='blue')
 
 plt.xlabel('Principal component 1')
 plt.ylabel('Principal component 2')
